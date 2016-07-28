513644cf9721c7b3ce3179b675c2528d7c27add3
Enzo Huaricapcha <lhuaricapcha@gmail.com>
2016-07-28 17:10:37 -0500

    archivos estaticos css y js, template de las vistas y modelo Pregunta

---
+++ app/Pregunta.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pregunta extends Model
+{
+    protected $table = 'cntbd_pregunta';
+    protected $primaryKey = 'I_CODPREG';
+    public $timestamps = false;
+}
---
+++ resources/views/preguntas/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layout')
+
+@section('content')
+
+<div class="box-principal">
+  <h3 class="titulo">Listado de preguntas<hr></h3>
+  <div class="panel panel-success">
+    <div class="panel-heading">
+      <h3 class="panel-title">Listado de preguntas</h3>
+    </div>
+    <div class="panel-body">
+      <table class="table table-striped table-hover ">
+      <thead>
+        <tr>
+          <th>Cod pregunta</th>
+          <th>Pregunta</th>
+          <th>Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach($preguntas as $pregunta)
+          <tr>
+            <td>
+              <a href="/preguntas/ver/{!!$pregunta->I_CODPREG!!}">{!!$pregunta->I_CODPREG!!}</a>
+            </td>
+            <td>
+              {!!$pregunta->V_DESPREG!!}
+            </td>
+            <td>
+              <a class="btn btn-warning" href="/preguntas/editar/{!!$pregunta->I_CODPREG!!}">Editar</a>
+              <a class="btn btn-danger" href="/preguntas/eliminar/{!!$pregunta->I_CODPREG!!}">Eliminar</a>
+            </td>
+          </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+    </div>
+  </div>
+</div>
+
+@endsection
