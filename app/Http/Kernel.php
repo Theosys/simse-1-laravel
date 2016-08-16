30b8f997e7d5b166fd0cb29f58c5a639b3cb180d
Enzo Huaricapcha <lhuaricapcha@gmail.com>
2016-08-16 13:39:01 -0500

    Manejo de roles con middleware, solo se puede limitar el acceso a las rutas especificadas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,5 +49,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'administrador' => \App\Http\Middleware\Administrador::class,
+        'monitor' => \App\Http\Middleware\Monitor::class,
     ];
 }
