b9eeefe9b96bb726d437ff879341e03a4bd56b67
Ronald N <rloyola@cenepred.gob.pe>
2016-09-01 09:07:48 -0500

    layout del btn search and icons menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -19,14 +19,14 @@
         @endif
 
         <!-- search form (Optional) -->
-        <form action="#" method="get" class="sidebar-form">
+        <!--form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="{{ trans('adminlte_lang::message.search') }}..."/>
               <span class="input-group-btn">
                 <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
               </span>
             </div>
-        </form>
+        </form-->
         <!-- /.search form -->
 
         <!-- Sidebar Menu -->
@@ -49,21 +49,21 @@
               <a href="#"><i class='fa fa-link'></i><span>Encuestas</span><i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="{{ url('/encuestas') }}">Registrar/Actualizar</a></li>
-                <li><a href="{{ url('/contactos') }}">Contactos</a></li>
+                <!--li><a href="{{ url('/contactos') }}">Contactos</a></li>
                 <li><a href="{{ url('/sectecnicos') }}">Secretarios Tec.</a></li>
                 <li><a href="{{ url('/respuestas') }}">Respuestas resumen</a></li>
                 <li><a href="{{ url('/seguimiento') }}">Seguimiento </a></li>
-                <li><a href="{{ url('/encuestas1/cobertura') }}">Cobertura </a></li>
+                <li><a href="{{ url('/encuestas1/cobertura') }}">Cobertura </a></li-->
               </ul>
             </li>
-            <li class="treeview">
+            <!--li class="treeview">
               <a href="#"><i class='fa fa-link'></i><span>Reportes</span><i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="{{ url('/reportes') }}">Resumen</a></li>
                 <li><a href="{{ url('/reportes/indicadores') }}">Indicadores</a></li>
                 <li><a href="{{ url('/reportes/indicadores/total') }}">Avance total indicadores</a></li>                
               </ul>
-            </li>   
+            </li-->   
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
