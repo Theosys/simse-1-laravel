4bef71e6f7aceff1276fdd5bd1deed3bd09098e3
Ronald N <rloyola@cenepred.gob.pe>
2016-09-15 19:15:31 -0500

    change pages home

--- resources/views/cenepred/cabeza.blade.php
+++ resources/views/cenepred/cabeza.blade.php
@@ -24,8 +24,8 @@
             		<ul class="nav navbar-nav" style="">		        	
 			        	<li><a href="#">Inicio</a></li>
 			        	<li><a href="{{ url('/glosario') }}">Glosario</a></li>
-			        	<li><a href="{{ url('/normativas') }}">Normativas</a></li>
-			        	<li><a href="{{ url('/contacto') }}">Contacto</a></li>
+			        	<li><a href="{{ url('/normativas') }}">Documentos Técnicos</a></li>
+			        	<li><a href="{{ url('/contacto') }}">Sugerencias</a></li>
 			        	<li><a href="{{ url('/directorio') }}">Directorio</a></li>
 			        	@if(Auth::user())
 			        	<li><a href="{{ url('/admin') }}">Administrador</a></li>			        				        	
