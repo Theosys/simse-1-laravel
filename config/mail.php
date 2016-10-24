691216b7a6032f8699360af8c6b51b0393d24348
Ronald N <rloyola@cenepred.gob.pe>
2016-10-24 16:23:27 -0500

    config mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'dimse@cenepred.gob.pe', 'name' => 'DIMSE - CENEPRED'],
 
     /*
     |--------------------------------------------------------------------------
