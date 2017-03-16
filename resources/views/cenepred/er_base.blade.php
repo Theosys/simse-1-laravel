6795f95f444b54ee35ddb6104d7734f529cee6d1
Ronald N <rloyola@cenepred.gob.pe>
2017-03-16 17:57:33 -0500

    timeline options

--- resources/views/cenepred/er_base.blade.php
+++ resources/views/cenepred/er_base.blade.php
@@ -3,6 +3,7 @@
 <head>	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="language" content="es">
+    <link rel="stylesheet" href="{{ asset('/cenepred/css/flat_and_modern.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('/cenepred/css/cenepred_main.css?ver=1.5') }}">
 	<script type="text/javascript" src="{{ asset('/cenepred/scripts/jquery-2.2.3.min.js') }}"></script>	
 </head>
@@ -12,21 +13,17 @@
  		<div id="map"></div>
  		<div id="capture">
  			  <div class="row survey-welcome">
- 			    <div class="col-sm-12 ">    
- 			        <p class="surveywelcome">
- 			            </p><p style="text-align: center;">&nbsp;<img alt="yes" src="http://dimse.cenepred.gob.pe/encuestas/upload/templates/cenepred/files/cenepred-ericka-hola.png" title="yes"></p>
-
-     			    <p style="text-align: center;"><strong><span style="font-size:14px;">Hola estimad@!!</span></strong></p>
-     			    <p>Si deseas información detallada por departamento y provincias debe hacer click en cada  región del mapa.</p>
+ 			    <div class="col-sm-12 ">
+                    <div class="col-sm-6">
+                        <p style="text-align: center;"><strong><span style="font-size:14px;">Hola estimad@!!</span></strong></p>
+                        <p>Si deseas información detallada por departamento y provincias debe hacer click en cada  región del mapa.</p>                    
+                    </div>
+                    <div class="col-sm-6">                    
+                        <p style="text-align: right;">&nbsp;<img class="erika-small" alt="erika-cenepred" src="http://dimse.cenepred.gob.pe/encuestas/upload/templates/cenepred/files/cenepred-ericka-hola.png" title="yes"></p>
+                    </div> 
                     <div>
                         <span class="er-nivel er_afectado"></span>  Departamento afectado  </br>
-                        <ul>
-                            <li>Ayacucho</li>
-                            <li>Apurimac</li>
-                            <li>Arequipa</li>
-                            <li>Cusco</li>
-                            <li>Puno</li>                            
-                        </ul>                
+                        <a class=" btn er-red" href="#">Ayacucho</a> <a href="">Apurimac</a> <a href="#">Arequipa</a> <a href="#">Cusco</a> <a href="#">Puno</a>
                     </div>
  			    </div>
  			  </div>        
--- resources/views/cenepred/tm_er.blade.php
+++ resources/views/cenepred/tm_er.blade.php
@@ -3,12 +3,11 @@
 @section('contenido')
 
 <link title="timeline-styles" rel="stylesheet" href="{{ asset('/plugins/timeline/css/timeline.css') }}">
-<link rel="stylesheet" href="{{ asset('/cenepred/css/flat_and_modern.css') }}">
 <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"></script>
 
 <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.18.1/vis.min.css">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.18.1/vis.min.js"></script> -->
-<h5 class="btn er">Escenario de Riesgos</h5>
+<h5 class="btn er">Escenario de Riesgos Ante la Temporada de Lluvias 2016 - 2017</h5>
 
 <div id="visualization"></div>
 <div id='timeline-embed' style="width: 100%; height: 600px"></div>
@@ -36,8 +35,14 @@
   // Create a Timeline
   var timeline = new vis.Timeline(container, items, options);*/
   //var timeline_json = make_the_json('http://dimse.cenepred.gob.pe/json/tm_example.json');
+  var options = {
+    hash_bookmark: false,    
+    start_at_end : true,//empezar en el ultimo 
+    language: 'es',
+    hash_bookmark: true
+  }
 
   timeline = new TL.Timeline('timeline-embed',
-        'https://docs.google.com/spreadsheets/d/1pcVvu9BduarvxyXbb-2b2QTEDc4vATK0ZFmhUokjJRk/pubhtml');
+        'https://docs.google.com/spreadsheets/d/1pcVvu9BduarvxyXbb-2b2QTEDc4vATK0ZFmhUokjJRk/pubhtml',options);
 </script>
 @endsection
